a8ae5bc926cc076acb163bdabbf77a7371dc4d51
BIANCA GONZALEZ <bgonzzz@BIANCAs-MacBook-Pro.local>
2021-04-13 23:36:43 -0700

    database added 1 truck

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import awsconfig from './aws-exports';
 import React, { useState, useEffect } from 'react';
@@ -30,19 +29,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Trucking App</h2>
       </header>
+      <div className="truckList">
+          { truck.map(truck => {
+            return(
+                <div>
+                  <div className="truckID">
+                    Truck ID: {truck.id}
+                  </div>
+                  <div className="driverName">
+                    Driver: {truck.lastName}, {truck.firstName}
+                  </div>
+                  <div className="driverVitals">
+                      Heart Rate: {truck.heartRate} | Oxygen: {truck.oxygen} | Blood Pressure: {truck.systolic}/{truck.diastolic} | Temperature: {truck.temperature}
+                  </div>
+                  <div className="truckHealth">
+                    Location: {truck.location} | Fuel Level: {truck.lowFluel} | Lamp: {truck.lampOut} | Fog Lamp: {truck.fogLamp} | Oil: {truck.oil} | Tire Pressure: {truck.tire} | Engine Temperature: {truck.engTemp} | Traction: {truck.traction} | Antilock Break: {truck.antilockBreak} | Traction Control: {truck.tractionControlMalfunction} | Engine: {truck.engineWarning} | Battery: {truck.battery} | Seat belt: {truck.seatBelt} | Air Bag: {truck.airbag} | Washer Fluid: {truck.washerFluid}
+                  </div>
+                </div>
+              )
+          })}
+      </div>
     </div>
   );
 }
