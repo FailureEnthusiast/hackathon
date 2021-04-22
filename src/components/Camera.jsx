68ea6ff59f77c4e9d56d97ebb1fd033edb32a097
BIANCA GONZALEZ <bgonzzz@BIANCAs-MacBook-Pro.local>
2021-04-22 14:05:37 -0700

    Camera.jsx table

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,48 +1,45 @@
-import awsconfig from '../aws-exports';
+//React
 import React, { useState, useEffect } from 'react';
-import { listTrucks } from '../graphql/queries';
-import { MDBContainer, MDBRow, MDBCol,MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
-// import { updateTrucks } from './graphql/mutations';
-// import { createTrucks } from './graphql/mutations';
-// import { deleteTrucks } from './graphql/mutations';
-import Amplify, { API, graphqlOperation, Storage } from 'aws-amplify';
-// import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 
+//AWS Amplify
+import awsconfig from '../aws-exports';
+import  { listTrucks } from './../graphql/queries';
+import Amplify, { API, graphqlOperation, Storage } from 'aws-amplify';
+import { AmplifyS3Image, withAuthenticator } from '@aws-amplify/ui-react';
+
+// import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(awsconfig);
 
 function Camera() {
     const [truck, setTrucks] = useState([]);
-    const [truckPlaying, setTruckPlaying] = useState('');
+    const [truckOn, setTruckOn] = useState('');
     const [truckURL, setTruckURL] = useState('');
-
 
     useEffect(()=> {
       fetchTrucks()
     }, []);
 
-  //   const toggleTruck = async idx => {
-  //     if (truckPlaying === idx) {
-  //         setTruckPlaying('');
-  //         return;
-  //     }
-    
-  //   const truckFilePath = truck[idx].filePath;
-  //   try {
-  //     const fileAccessURL = await Storage.get(truckFilePath, { expires: 60 });
-  //     console.log('access url', fileAccessURL);          
-  //     setTruckPlaying(idx);
-  //     setTruckURL(fileAccessURL);
-  //         return;
-  //    } catch (error) {
-  //       console.error('error accessing the file from s3', error);
-  //       setTruckURL('');         
-  //      setTruckPlaying('');
-  //     };
+    const toggleTruck = async idx => {
+      if (truckOn === idx) {
+          setTruckOn('');
+          return;
+      }
 
-  //     setTruckPlaying(idx);
-  //     return
-  // }
+        //get file path from truck array
+        const truckFilePath = truck[idx].filePath;
+        try {
+          const fileAccessURL = await Storage.get(truckFilePath, { expires: 60 });
+          console.log('access url', fileAccessURL);          
+          setTruckOn(idx);
+          setTruckURL(fileAccessURL);
+              return
+        } catch (error) {
+            console.log('error accessing the file from s3', error);
+            setTruckURL('');         
+            setTruckOn('');
+          }
+      }
 
     const fetchTrucks = async () => {
         try {
@@ -56,42 +53,81 @@ function Camera() {
     };
 
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-      <div className="truckList">
-          { truck.map((truck, idx) => {
-            return(
-                <div key={`truck${idx}`}>
-                  <div className="dashboardImg">
-
-                    <img src={truck.img} height='60%' width='60%'></img>
-                  </div>
-                  <div className="truckHealth">
-                    Fuel Level: {truck.lowFuel} 
-                    | Lamp: {truck.lampOut} 
-                    | Fog Lamp: {truck.fogLamp} 
-                    | Oil: {truck.oil} 
-                    | Tire Pressure: {truck.tire} 
-                    | Engine Temperature: {truck.engTemp} 
-                    | Traction: {truck.traction} 
-                    | Antilock Break: {truck.antilockBreak} 
-                    | Traction Control: {truck.tractionControlMalfunction} 
-                    | Engine: {truck.engineWarning} 
-                    | Battery: {truck.battery} 
-                    | Seat belt: {truck.seatBelt} 
-                    | Air Bag: {truck.airbag} 
-                    | Washer Fluid: {truck.washerFluid}
-                  </div>
-                 <div className="div">
+              <div className='truckCard'>
+                  <p><AmplifyS3Image path={truck.filePath} /></p>
+                  <table>
+                    <thead>
+                      <tr>
+                          <th>Indicator</th>
+                          <th>Value</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Fuel Level</td>
+                        <td>{truck.lowFuel}</td>
+                      </tr>
+                      <tr>
+                        <td>Speed</td>
+                        <td>{truck.speed}</td>
+                      </tr>
+                      <tr>
+                        <td>Lamp Out</td>
+                        <td>{truck.lampOut}</td>
+                      </tr>
+                      <tr>
+                        <td>Fog Lamp On</td>
+                        <td>{truck.fogLamp}</td>
+                      </tr>
+                      <tr>
+                        <td>Oil</td>
+                        <td>{truck.oil}</td>
+                      </tr>
+                      <tr>
+                        <td>Tire Pressure</td>
+                        <td>{truck.tire}</td>
+                      </tr>
+                      <tr>
+                        <td>Engine Temperature</td>
+                        <td>{truck.engTemp}</td>
+                      </tr>
+                      <tr>
+                        <td>Traction</td>
+                        <td>{truck.traction}</td>
+                      </tr>
+                      <tr>
+                        <td>Antilock Break On</td>
+                        <td>{truck.antilockBreak}</td>
+                      </tr>
+                      <tr>
+                        <td>Traction Control Malfunction</td>
+                        <td>{truck.tractionControlMalfunction}</td>
+                      </tr>
+                      <tr>
+                        <td>Engine Warning</td>
+                        <td>{truck.engineWarning}</td>
+                      </tr>
+                      <tr>
+                        <td>Battery</td>
+                        <td>{truck.battery}</td>
+                      </tr>
+                      <tr>
+                        <td>Seat Belt On </td>
+                        <td>{truck.seatBelt}</td>
+                      </tr>
+                      <tr>
+                        <td>Air Bag</td>
+                        <td>{truck.airbag}</td>
+                      </tr>
+                      <tr>
+                        <td>Washer Fluid Low</td>
+                        <td>{truck.washerFluid}</td>
+                      </tr>
+                    </tbody>
+                  </table>
                  </div>
-                </div>
-              )
-          })}
-      </div>
-    </div>
+    
   );
-};
+}
 
 export default Camera;
